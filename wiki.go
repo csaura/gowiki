0de02a6c04e05c78f207fdfc194b4b48d6fa1dfe
Carlos Saura <saurastar@gmail.com>
2014-02-07 00:21:09 +0100

    Wiki on files example

---
+++ wiki.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+  "regexp"
+  "errors"
+  "html/template"
+  "io/ioutil"
+  "net/http"
+)
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+type Page struct {
+  Title string
+  Body []byte
+}
+
+func (page *Page) save() error {
+  filename := page.Title + ".txt"
+  return ioutil.WriteFile(filename, page.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+  filename := title + ".txt"
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
+  return &Page{Title: title, Body: body}, nil
+}
+
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title")
+  }
+  return m[2], nil
+}
+
+func viewHandler(w http.ResponseWriter, request *http.Request, title string) {
+  p, err := loadPage(title)
+  if err != nil {
+    http.Redirect(w, request, "/edit/"+title, http.StatusFound)
+    return
+  }
+  renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+  page, err := loadPage(title)
+  if err != nil {
+    page = &Page{Title: title}
+  }
+  renderTemplate(w, "edit", page)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  err := p.save()
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+  http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return
+    }
+    fn(w, r, m[2])
+  }
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+  err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
+
+func main() {
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
+  http.ListenAndServe(":8080", nil)
+}
